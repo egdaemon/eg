699dd38950a3c5e7744ca2a72eeebe52592e4883
James Lawrence <jljatone@gmail.com>
2023-11-18 14:57:40 -0500

    logging

--- compile/compile.go
+++ compile/compile.go
@@ -16,13 +16,13 @@ func Run(ctx context.Context, module string, output string) (err error) {
 		return err
 	}
 
-	// cmd := exec.CommandContext(ctx, "tinygo", "build", "-tags", "wasm", "-target", "wasi", "-o", output, module)
-	// cmd.Env = append(os.Environ(), "GOOS=wasip1", "GOARCH=wasm")
 	cmd := exec.CommandContext(ctx, "go", "build", "-trimpath", "-o", output, module)
 	cmd.Env = append(os.Environ(), "GOOS=wasip1", "GOARCH=wasm")
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
+
+	log.Println("compiling", cmd.String())
 
 	return cmd.Run()
 }

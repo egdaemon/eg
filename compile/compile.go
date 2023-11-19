c1190614c6cd4977f310c047ecad2f3a394c5935
James Lawrence <jljatone@gmail.com>
2023-11-19 13:25:11 -0500

    adjust module path

--- compile/compile.go
+++ compile/compile.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 )
 
 func Run(ctx context.Context, dir, module string, output string) (err error) {
@@ -16,7 +17,7 @@ func Run(ctx context.Context, dir, module string, output string) (err error) {
 		return err
 	}
 
-	cmd := exec.CommandContext(ctx, "go", "build", "-trimpath", "-o", output, module)
+	cmd := exec.CommandContext(ctx, "go", "build", "-trimpath", "-o", output, strings.TrimPrefix(dir, module))
 	cmd.Env = append(os.Environ(), "GOOS=wasip1", "GOARCH=wasm")
 	cmd.Dir = dir
 	cmd.Stdin = os.Stdin

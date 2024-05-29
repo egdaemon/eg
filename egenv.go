eff33f57a1707eb4eb760c050e42adc19445bd1c
James Lawrence <jljatone@gmail.com>
2024-05-29 06:41:39 -0400

    reset api endpoints

--- egenv.go
+++ egenv.go
@@ -1,22 +1,22 @@
 package eg
 
 const (
-	EnvEGAPIHost = "EG_API_HOST"
-	// EnvEGAPIHostDefault = "https://api.egdaemon.com"
-	EnvEGAPIHostDefault = "https://localhost:8081"
+	EnvEGAPIHost        = "EG_API_HOST"
+	EnvEGAPIHostDefault = "https://api.egdaemon.com"
+	// EnvEGAPIHostDefault = "https://localhost:8081"
 )
 
 const (
 	EnvEGSSHHost          = "EG_SSH_REVERSE_PROXY_HOST"
 	EnvEGSSHProxyDisabled = "EG_SSH_REVERSE_PROXY_DISABLED"
-	// EnvEGSSHHostDefault   = "api.egdaemon.com:8090"
-	EnvEGSSHHostDefault = "localhost:8090"
+	EnvEGSSHHostDefault   = "api.egdaemon.com:8090"
+	// EnvEGSSHHostDefault = "localhost:8090"
 )
 
 const (
-	EnvEGConsoleHost = "EG_CONSOLE_HOST"
-	// EnvEGConsoleHostDefault = "https://console.egdaemon.com"
-	EnvEGConsoleHostDefault = "https://localhost:8080"
+	EnvEGConsoleHost        = "EG_CONSOLE_HOST"
+	EnvEGConsoleHostDefault = "https://console.egdaemon.com"
+	// EnvEGConsoleHostDefault = "https://localhost:8080"
 )
 
 // Logging settings

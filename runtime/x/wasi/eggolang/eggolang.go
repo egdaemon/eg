b9bf2ae9d5a4225b4f7ffb007b4967bdd827688a
James Lawrence <jljatone@gmail.com>
2024-11-13 12:26:06 -0500

    update path used to locate go mods

--- runtime/x/wasi/eggolang/eggolang.go
+++ runtime/x/wasi/eggolang/eggolang.go
@@ -10,11 +10,12 @@ import (
 	"github.com/egdaemon/eg/internal/execx"
 	"github.com/egdaemon/eg/internal/modfilex"
 	"github.com/egdaemon/eg/runtime/wasi/eg"
+	"github.com/egdaemon/eg/runtime/wasi/egenv"
 )
 
 func AutoCompile() eg.OpFn {
 	return eg.OpFn(func(ctx context.Context, _ eg.Op) error {
-		for gomod := range modfilex.FindModules(".") {
+		for gomod := range modfilex.FindModules(egenv.RootDirectory()) {
 			if err := execx.MaybeRun(exec.CommandContext(ctx, "go", "build", fmt.Sprintf("%s/...", filepath.Dir(gomod)))); err != nil {
 				return errorsx.Wrap(err, "unable to compile")
 			}
@@ -25,7 +26,7 @@ func AutoCompile() eg.OpFn {
 
 func AutoTest() eg.OpFn {
 	return eg.OpFn(func(ctx context.Context, _ eg.Op) error {
-		for gomod := range modfilex.FindModules(".") {
+		for gomod := range modfilex.FindModules(egenv.RootDirectory()) {
 			if err := execx.MaybeRun(exec.CommandContext(ctx, "go", "test", fmt.Sprintf("%s/...", filepath.Dir(gomod)))); err != nil {
 				return errorsx.Wrap(err, "unable to run tests")
 			}

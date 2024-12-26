a89c2a146877e78e57ed3ae626ec499acf3a3c3d
James Lawrence <jljatone@gmail.com>
2024-12-26 12:21:46 -0500

    add network test

--- .eg/tests/metrics/main.go
+++ .eg/tests/metrics/main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"io"
 	"log"
 	"math/rand"
-	"net"
-	"net/http"
 
 	"github.com/egdaemon/eg/runtime/wasi/eg"
 	"github.com/egdaemon/eg/runtime/wasi/egenv"
@@ -24,88 +20,6 @@ func automcpu() MetricCPU {
 		Load: rand.Float32(),
 	}
 }
-
-func listentcp(network, address string) net.Listener {
-	li, err := net.Listen(network, address)
-	if err != nil {
-		panic(err)
-	}
-
-	go func() {
-		for conn, err := li.Accept(); err == nil; conn, err = li.Accept() {
-			server, client := net.Pipe()
-			go func(c net.Conn) {
-				if _, err := io.Copy(c, server); err != nil {
-					log.Println("server copy failed", err)
-				}
-			}(conn)
-			go func(c net.Conn) {
-				defer c.Close()
-				if _, err := io.Copy(client, c); err != nil {
-					log.Println("client copy failed", err)
-				}
-			}(conn)
-		}
-	}()
-
-	return li
-}
-
-func checkTransfer(ctx context.Context, li net.Listener) error {
-	var (
-		buf []byte = make([]byte, 128)
-	)
-
-	conn, err := net.Dial(li.Addr().Network(), li.Addr().String())
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-
-	if _, err = conn.Write([]byte("hello world")); err != nil {
-		return err
-	}
-
-	if n, err := conn.Read(buf); err != nil {
-		return err
-	} else if v := string(buf[:n]); v != "hello world" {
-		return fmt.Errorf("recieved %s expected %s", v, "hello world")
-	} else {
-		log.Println("transferred", string(buf[:n]))
-	}
-
-	return nil
-}
-
-func TCPTest(ctx context.Context, op eg.Op) error {
-	if err := checkTransfer(ctx, listentcp("tcp", ":0")); err != nil {
-		return err
-	}
-	return nil
-}
-
-func DNSTCPResolveTest(ctx context.Context, op eg.Op) error {
-	ip, err := net.ResolveTCPAddr("tcp", "www.google.com:443")
-	if err != nil {
-		return err
-	}
-	log.Println("IP ADDRESS", ip.IP, ip.Port)
-	return nil
-}
-
-func HTTPTest(ctx context.Context, op eg.Op) error {
-	rsp, err := http.Get("https://localhost:8080")
-	if err != nil {
-		return err
-	}
-
-	if rsp.StatusCode != http.StatusOK {
-		return fmt.Errorf("unexpected status code: %d", rsp.StatusCode)
-	}
-
-	return nil
-}
-
 func Debug(ctx context.Context, op eg.Op) error {
 	log.Println("debug initiated")
 	defer log.Println("debug completed")
@@ -129,10 +43,7 @@ func main() {
 
 	err := eg.Perform(
 		ctx,
-		// Debug,
-		DNSTCPResolveTest,
-		// TCPTest,
-		// HTTPTest,
+		Debug,
 	)
 
 	if err != nil {

1753ef14313199bc3446660b8fc95cb9412b9c40
James Lawrence <jljatone@gmail.com>
2025-01-06 10:58:04 -0500

    fix stress test

--- .eg/tests/metrics/main.go
+++ .eg/tests/metrics/main.go
@@ -30,7 +30,7 @@ func Debug(ctx context.Context, op eg.Op) error {
 		shell.New("pwd"),
 		shell.Newf("truncate --size 0 %s", egenv.RuntimeDirectory("environ.env")).Lenient(true),
 		shell.Newf("tree -L 2 %s", egenv.RootDirectory()),
-		shell.Newf("apt-get install stress"),
+		shell.Newf("apt-get install stress").Privileged(),
 		shell.Newf("stress -t 5 -c %d", 24),
 		// shell.Newf("stress -t 5 -m %d", 24), // requires 6GB of ram
 	)

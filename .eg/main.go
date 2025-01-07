0d0c622b54cad0001d7b50e1c44f846c4f740bf8
James Lawrence <jljatone@gmail.com>
2025-01-07 14:09:36 -0500

    cleanup debugging code

--- .eg/main.go
+++ .eg/main.go
@@ -7,32 +7,8 @@ import (
 	"github.com/egdaemon/eg/runtime/wasi/eg"
 	"github.com/egdaemon/eg/runtime/wasi/egenv"
 	"github.com/egdaemon/eg/runtime/wasi/eggit"
-	"github.com/egdaemon/eg/runtime/wasi/shell"
 	"github.com/egdaemon/eg/runtime/x/wasi/eggolang"
 )
-
-func Test(ctx context.Context, _ eg.Op) error {
-	return shell.Run(
-		ctx,
-		shell.New("ls -lha /opt/eg"),
-		// shell.Newf(
-		// 	"chmod -R 0770 %s",
-		// 	egenv.RootDirectory(),
-		// ).Privileged(),
-		// shell.New("ls -lha /opt/eg"),
-		// shell.New("git fsck --full | head -25").Privileged(),
-		// shell.New("git fsck --full | head -25"),
-		// shell.New("git status --porcelain | head -5"),
-		// shell.New("false"),
-	)
-}
-
-func Prepare(ctx context.Context, _ eg.Op) error {
-	return shell.Run(
-		ctx,
-		// shell.Newf("git config safe.directory"),
-	)
-}
 
 func main() {
 	log.SetFlags(log.Lshortfile | log.LUTC | log.Ltime)
@@ -41,20 +17,13 @@ func main() {
 
 	err := eg.Perform(
 		ctx,
-		Prepare,
 		eggit.AutoClone,
 		eg.Build(
 			eg.DefaultModule(),
 		),
-		Test,
-		eggolang.AutoCompile(
-			eggolang.CompileOption.Tags("no_duckdb_arrow"),
-			// eggolang.CompileOption.Debug(true),
-		),
 		eg.Module(
 			ctx,
 			eg.DefaultModule(),
-			Test,
 			eggolang.AutoCompile(
 				eggolang.CompileOption.Tags("no_duckdb_arrow"),
 				// eggolang.CompileOption.Debug(true),

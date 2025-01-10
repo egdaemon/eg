81459c518ef0f22b0c4b3c017a2a464bafb9d5ab
James Lawrence <jljatone@gmail.com>
2025-01-09 19:58:50 -0500

    remove insecure hint for dev environment, no longer needed

--- cmd/eg/daemons/registration.go
+++ cmd/eg/daemons/registration.go
@@ -88,12 +88,8 @@ func Register(global *cmdopts.Global, tlsc *cmdopts.TLSConfig, runtimecfg *cmdop
 		}
 
 		if ts := time.Now(); authzedts.After(ts) {
-			insecure := ""
-			if tlsc.Insecure {
-				insecure = " --insecure"
-			}
 			debugx.Println("authzed timestamp", authzedts, "<", ts)
-			log.Printf("waiting for registration to be accepted. run `eg actl authorize id '%s'%s` to accept\n", md5x.String(fingerprint), insecure)
+			log.Printf("waiting for registration to be accepted. run `eg actl authorize id '%s'` to accept\n", md5x.String(fingerprint))
 			continue
 		}
 

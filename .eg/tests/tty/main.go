afb06bedd20a3d9c33a6c7c7e8438aef15e2ff16
James Lawrence <jljatone@gmail.com>
2025-01-22 00:18:09 -0500

    tty tests

--- .eg/tests/tty/main.go
+++ .eg/tests/tty/main.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"context"
+	"crypto/md5"
 	"log"
+	"os"
+	"sort"
+	"strings"
 	"time"
 
 	"github.com/egdaemon/eg/runtime/wasi/eg"
 	"github.com/egdaemon/eg/runtime/wasi/egenv"
 	"github.com/egdaemon/eg/runtime/wasi/shell"
+	"github.com/egdaemon/eg/runtime/x/wasi/egbug"
 )
+
+// Ensure that the environment is as expected.
+func EnvTest(ctx context.Context, _ eg.OpFn) error {
+	const expected = ""
+	environ := os.Environ()
+	sort.Stable(sort.StringSlice(environ))
+	digest := md5.Sum([]byte(strings.Join(environ, "")))
+	return shell.Run(
+		ctx,
+		shell.New("env | sort"),
+		shell.Newf("test %s -eq %s", digest, expected),
+	)
+}
 
 func main() {
 	ctx, done := context.WithTimeout(context.Background(), egenv.TTL())
@@ -16,7 +34,12 @@ func main() {
 
 	err := eg.Perform(
 		ctx,
-		shell.Op(shell.New("systemctl status systemd-resolved.service").Privileged().Timeout(time.Second)),
+		egbug.SystemInit,
+		egbug.Env,
+		egbug.Users,
+		egbug.FileTree,
+		eg.Build(eg.DefaultModule()),
+		eg.Module(ctx, eg.DefaultModule(), shell.Op(shell.New("systemctl status systemd-resolved.service").Privileged().Timeout(time.Second))),
 	)
 
 	if err != nil {

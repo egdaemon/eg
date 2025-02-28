dde8ddcf170e78f3acec69c4121e285f59a317cb
James Lawrence <jljatone@gmail.com>
2025-02-28 15:26:42 -0500

    update flatpak module

--- runtime/x/wasi/egflatpak/egflatpak.go
+++ runtime/x/wasi/egflatpak/egflatpak.go
@@ -10,7 +10,9 @@ import (
 	"github.com/egdaemon/eg/internal/errorsx"
 	"github.com/egdaemon/eg/internal/langx"
 	"github.com/egdaemon/eg/runtime/wasi/eg"
+	"github.com/egdaemon/eg/runtime/wasi/egenv"
 	"github.com/egdaemon/eg/runtime/wasi/shell"
+	"github.com/egdaemon/eg/runtime/x/wasi/egfs"
 	"github.com/google/uuid"
 	"gopkg.in/yaml.v3"
 )
@@ -18,15 +20,17 @@ import (
 // Manifests describe how to build the application.
 // see https://docs.flatpak.org/en/latest/manifests.html
 type Manifest struct {
-	ID      string `yaml:"id"`
-	Runtime `yaml:",inline"`
-	SDK     `yaml:",inline"`
-	Command string   `yaml:"command"`
-	Modules []Module `yaml:"modules"`
+	ID         string `yaml:"id"`
+	Runtime    `yaml:",inline"`
+	SDK        `yaml:",inline"`
+	Command    string   `yaml:"command"`
+	Modules    []Module `yaml:"modules"`
+	FinishArgs []string `yaml:"finish-args"`
 }
 
 type SDK struct {
-	ID string `yaml:"sdk"`
+	ID      string `yaml:"sdk"`
+	Version string `yaml:"-"`
 }
 
 type Runtime struct {
@@ -36,8 +40,9 @@ type Runtime struct {
 
 type Source struct {
 	Type        string   `yaml:"type"`
-	Destination string   `yaml:"dest-filename"`
-	Commands    []string `yaml:"commands"`
+	Destination string   `yaml:"dest-filename,omitempty"`
+	Path        string   `yaml:"path,omitempty"` // used by directory source.
+	Commands    []string `yaml:"commands,omitempty"`
 }
 
 type Module struct {
@@ -54,30 +59,93 @@ var Option = options(nil)
 
 func (t options) CopyModule(dir string) options {
 	return append(t, func(b *Builder) {
-		b.Modules = append(b.Modules, Module{Name: "copy", BuildSystem: "simple", Commands: []string{
-			"ls -lha .",
-			fmt.Sprintf("cp -r %s .", dir),
-			"ls -lha .",
-			"echo copy done",
-		}})
+		b.Modules = append(
+			b.Modules,
+			Module{
+				Name:        "copy",
+				BuildSystem: "simple",
+				Commands: []string{
+					"cp -r . /app/bin",
+				},
+				Sources: []Source{
+					{Type: "dir", Path: dir},
+				},
+			})
 	})
 }
 
+// Specify the runtime to build from. `flatpak list --runtime`
 func (t options) Runtime(name, version string) options {
 	return append(t, func(b *Builder) {
 		b.Runtime = Runtime{ID: name, Version: version}
 	})
 }
 
+// Specify the sdk to build with.
+func (t options) SDK(name, version string) options {
+	return append(t, func(b *Builder) {
+		b.SDK = SDK{ID: name, Version: version}
+	})
+}
+
+// Enable gpu access
+func (t options) AllowDRI() options {
+	return append(t, func(b *Builder) {
+		b.FinishArgs = append(b.FinishArgs, "--device=dri")
+	})
+}
+
+// enable access to the wayland socket.
+func (t options) AllowWayland() options {
+	return append(t, func(b *Builder) {
+		b.FinishArgs = append(b.FinishArgs, "--socket=wayland")
+	})
+}
+
+// grant access to the network.
+func (t options) AllowNetwork() options {
+	return append(t, func(b *Builder) {
+		b.FinishArgs = append(b.FinishArgs, "--share=network")
+	})
+}
+
+// grant access to the downloads directory.
+func (t options) AllowDownload() options {
+	return append(t, func(b *Builder) {
+		b.FinishArgs = append(b.FinishArgs, "--filesystem=xdg-download")
+	})
+}
+
+// grant access to the downloads directory.
+func (t options) AllowVideos() options {
+	return append(t, func(b *Builder) {
+		b.FinishArgs = append(b.FinishArgs, "--filesystem=xdg-videos")
+	})
+}
+
+// grant access to the downloads directory.
+func (t options) AllowMusic() options {
+	return append(t, func(b *Builder) {
+		b.FinishArgs = append(b.FinishArgs, "--filesystem=xdg-music")
+	})
+}
+
+// escape hatch for finish-args.
+func (t options) Allow(s ...string) options {
+	return append(t, func(b *Builder) {
+		b.FinishArgs = append(b.FinishArgs, s...)
+	})
+}
+
 // configure the manifest for building the flatpak, but default it'll copy everything in the current
 // directory in an rsync like manner.
-func New(id string, options ...option) *Builder {
+func New(id string, command string, options ...option) *Builder {
 	return langx.Autoptr(langx.Clone(Builder{
 		Manifest: Manifest{
 			ID:      id,
-			Runtime: Runtime{ID: "org.freedesktop.Platform", Version: "freedesktop-sdk-23.08.28"},
-			SDK:     SDK{ID: "org.freedesktop.Sdk"},
-			Command: "egflatpak.app",
+			Runtime: Runtime{ID: "org.freedesktop.Platform", Version: "24.08"},
+			SDK:     SDK{ID: "org.freedesktop.Sdk", Version: "24.08"},
+			Command: command,
 			Modules: []Module{},
 		},
 	}, options...))
@@ -85,6 +153,15 @@ func New(id string, options ...option) *Builder {
 
 // Build the flatpak
 func Build(ctx context.Context, runtime shell.Command, b *Builder) error {
+	var (
+		sysdir  = egenv.CacheDirectory(".eg", "flatpak-system")
+		userdir = egenv.CacheDirectory(".eg", "flatpak-user")
+	)
+
+	if err := egfs.MkDirs(0755, sysdir, userdir); err != nil {
+		return err
+	}
+
 	dir, err := os.MkdirTemp(".", "flatpak.build.*")
 	if err != nil {
 		return err
@@ -96,16 +173,17 @@ func Build(ctx context.Context, runtime shell.Command, b *Builder) error {
 		return err
 	}
 
+	runtime = runtime.Environ("FLATPAK_SYSTEM_DIR", sysdir).
+		Environ("FLATPAK_USER_DIR", userdir).
+		Privileged()
+
 	return shell.Run(
 		ctx,
-		runtime.New("id"),
-		runtime.New("chown -R egd:egd .").Privileged(),
-		runtime.Newf("chown egd:egd %s", manifestpath).Privileged(),
-		runtime.New("tree -L 2 ."),
-		runtime.New("pwd"),
-		// runtime.New("which flatpak-builder"),
-		runtime.Newf("ls -lha %s", manifestpath),
-		runtime.Newf("flatpak-builder --force-clean %s %s", dir, manifestpath),
+		runtime.Newf("cat %s", manifestpath),
+		runtime.New("flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo"),
+		runtime.Newf("flatpak install --system --assumeyes --include-sdk flathub %s//%s", b.Runtime.ID, b.Runtime.Version),
+		runtime.Newf("flatpak install --system --assumeyes flathub %s//%s", b.SDK.ID, b.SDK.Version),
+		runtime.Newf("flatpak-builder --system --install-deps-from=flathub --install --force-clean %s %s", dir, manifestpath),
 	)
 }
 

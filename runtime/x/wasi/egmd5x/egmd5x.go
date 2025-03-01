9790eaadade81f2f192f4b60bb21fad497ae9cb9
James Lawrence <jljatone@gmail.com>
2025-03-01 10:46:19 -0500

    expose md5x utility functions

--- runtime/x/wasi/egmd5x/egmd5x.go
+++ runtime/x/wasi/egmd5x/egmd5x.go
@@ -1,36 +1,28 @@
-package md5x
+package egmd5x
 
 import (
-	"crypto/md5"
-	"encoding/hex"
 	"hash"
 
-	"github.com/gofrs/uuid"
+	"github.com/egdaemon/eg/internal/md5x"
 )
 
 // digest the provided contents and return the resulting hash.
 // if an error occurs during hashing then a nil value is returned.
 func Digest[T string | []byte](b T) hash.Hash {
-	v := md5.New()
-	y := []byte(b)
-	if n, err := v.Write(y); err != nil || n < len(y) {
-		return nil
-	}
-
-	return v
+	return md5x.Digest(b)
 }
 
-// String to md5 uuid encoded string
+// String to md5 uuid encoded string, shorthand for egdmdx.FormatString(egmd5x.Digest(v))
 func String(s string) string {
-	return FormatString(Digest(s))
+	return md5x.String(s)
 }
 
 // format md5 hash to a uuid encoded string
 func FormatString(m hash.Hash) string {
-	return uuid.FromBytesOrNil(m.Sum(nil)).String()
+	return md5x.FormatString(m)
 }
 
 // format md5 hash to a hex encoded string
 func FormatHex(m hash.Hash) string {
-	return hex.EncodeToString(m.Sum(nil))
+	return md5x.FormatHex(m)
 }

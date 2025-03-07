684c9491513b68b67945e962144e1c5272c826fb
James Lawrence <jljatone@gmail.com>
2025-03-07 07:43:56 -0500

    fix pid limit for local compute

--- runners/runner.option.linux.go
+++ runners/runner.option.linux.go
@@ -8,6 +8,7 @@ func AgentOptionHostOS() AgentOption {
 		AgentOptionCommandLine("--cap-add", "NET_ADMIN"), // required for loopback device creation inside the container
 		AgentOptionCommandLine("--cap-add", "SYS_ADMIN"), // required for rootless container building https://github.com/containers/podman/issues/4056#issuecomment-612893749
 		AgentOptionCommandLine("--device", "/dev/fuse"),  // required for rootless container building https://github.com/containers/podman/issues/4056#issuecomment-612893749
+		AgentOptionCommandLine("--pids-limit", "-1"),     // ensure we dont run into pid limits.
 	)
 }
 

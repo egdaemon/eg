05cc8bfb2efedce1ab3acfe926310c24d35734cc
James Lawrence <jljatone@gmail.com>
2025-03-27 11:01:41 -0400

    update github-cli repository

--- .eg/debuild/egbootstrap/rootfs/usr/share/eg/install/github.sh
+++ .eg/debuild/egbootstrap/rootfs/usr/share/eg/install/github.sh
@@ -3,5 +3,5 @@ set -e
 
 echo "installing github cli repository ${1}"
 
-curl https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee ${1}/usr/share/keyrings/githubcli-archive-keyring.gpg
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o ${1}/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null

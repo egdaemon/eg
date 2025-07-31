ed041614401cfc708f25066eb9a02b54693e86e4
James Lawrence <james@egdaemon.com>
2025-07-31 13:18:41 -0400

    fix typo in darwin warning

--- runners/runner.option.darwin.go
+++ runners/runner.option.darwin.go
@@ -9,7 +9,7 @@ import (
 )
 
 func AgentOptionHostOS() AgentOption {
-	log.Println(aurora.NewAurora(true).Red("darwin is currently in alpha host, you may experience some functionality issues, please report your findings to us for support/fixes."))
+	log.Println(aurora.NewAurora(true).Red("darwin is currently an alpha host, you may experience some functionality issues, please report your findings to us for support/fixes."))
 	return AgentOptionCompose(
 		AgentOptionCommandLine("--userns", "host"),   // properly map host user into containers.
 		AgentOptionCommandLine("--privileged"),       // darwin permission issues.

e981a5ee3838d60b3ad79589b5b1158d579399fb
James Lawrence <james@egdaemon.com>
2025-07-31 14:13:33 -0400

    debug defaultgroup

--- runtime/wasi/shell/shell_darwin.go
+++ runtime/wasi/shell/shell_darwin.go
@@ -1,12 +1,15 @@
 package shell
 
 import (
+	"log"
 	"os/user"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/egdaemon/eg/internal/stringsx"
 )
 
 func defaultgroup(u *user.User) string {
+	log.Println("defaultgroup darwin", spew.Sdump(u))
 	if stringsx.Present(u.Username) {
 		return "staff"
 	}
--- runtime/wasi/shell/shell_unix.go
+++ runtime/wasi/shell/shell_unix.go
@@ -2,8 +2,14 @@
 
 package shell
 
-import "os/user"
+import (
+	"log"
+	"os/user"
+
+	"github.com/davecgh/go-spew/spew"
+)
 
 func defaultgroup(u *user.User) string {
+	log.Println("defaultgroup unix", spew.Sdump(u))
 	return u.Username
 }

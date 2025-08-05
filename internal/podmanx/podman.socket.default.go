6d5c3256bd7bfeee5f00a9dd28836f0946c4e4c7
James Lawrence <james@egdaemon.com>
2025-08-05 18:30:00 -0400

    ensure podman.socket is enable for users by default.

--- internal/podmanx/podman.socket.default.go
+++ internal/podmanx/podman.socket.default.go
@@ -4,18 +4,19 @@ package podmanx
 
 import (
 	"fmt"
+	"log"
 	"path/filepath"
 
 	"github.com/egdaemon/eg/internal/fsx"
+	"github.com/egdaemon/eg/internal/stringsx"
 	"github.com/egdaemon/eg/internal/userx"
 )
 
 func DefaultSocket() string {
 	u := userx.CurrentUserOrDefault(userx.Root())
 	upath := filepath.Join("/var", "run", "user", u.Uid, "podman", "podman.sock")
-	socketpath := fsx.LocateFirst(
-		upath,
-		filepath.Join("/run", "podman", "podman.sock"),
-	)
+	rpath := filepath.Join("/run", "podman", "podman.sock")
+	log.Println("DERP DERP", upath, rpath)
+	socketpath := stringsx.DefaultIfBlank(fsx.LocateFirst(upath, rpath), upath)
 	return fmt.Sprintf("unix://%s", socketpath)
 }

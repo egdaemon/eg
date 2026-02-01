dec175574551b2c9f351b540e1408a4256fbef38
James Lawrence <jljatone@gmail.com>
2026-02-01 09:28:47 -0500

    update bootstrap command.
    handles runtime settings.

--- cmd/eg/actl/bootstrap.env.go
+++ cmd/eg/actl/bootstrap.env.go
@@ -9,7 +9,6 @@ import (
 	"runtime"
 	"strconv"
 
-	"github.com/dustin/go-humanize"
 	"github.com/egdaemon/eg/cmd/cmdopts"
 	"github.com/egdaemon/eg/internal/bytesx"
 	"github.com/egdaemon/eg/internal/cryptox"
@@ -41,13 +40,14 @@ func (t BootstrapEnvRunner) Run(gctx *cmdopts.Global) (err error) {
 }
 
 type BootstrapEnvDaemon struct {
-	runtimecfg cmdopts.RuntimeResources
-	AccountID  string `name:"account" help:"account to register runner with" default:"${vars_account_id}" required:"true"`
-	Seed       string `name:"seed" help:"used to ensure a consistent secret is used, this is a sensitive value" placeholder:"00000000-0000-0000-0000-000000000000"`
+	cmdopts.RuntimeResources
+	AccountID string `name:"account" help:"account to register runner with" default:"${vars_account_id}" required:"true"`
+	Seed      string `name:"seed" help:"used to ensure a consistent secret is used, this is a sensitive value" placeholder:"00000000-0000-0000-0000-000000000000"`
+	Workers   uint64 `name:"workers" help:"specify the maximum concurrent workload capacity"`
 }
 
 func (t BootstrapEnvDaemon) Run(gctx *cmdopts.Global) (err error) {
-	memory := numericx.Max(uint64(t.runtimecfg.Memory), uint64(float64(memory.TotalMemory())*0.9))
+	memory := numericx.Max(uint64(t.Memory), uint64(float64(memory.TotalMemory())*0.9))
 	prng := cryptox.NewChaCha8(langx.FirstNonZero(t.Seed, uuid.Must(uuid.NewV4()).String()))
 
 	seed, err := io.ReadAll(io.LimitReader(prng, rand.New(prng).Int64N(128)))
@@ -55,15 +55,23 @@ func (t BootstrapEnvDaemon) Run(gctx *cmdopts.Global) (err error) {
 		return errorsx.Wrap(err, "failed to generate entropy")
 	}
 
-	return envx.Build().Var(
+	environ := envx.Build().Var(
 		"EG_ACCOUNT", t.AccountID,
 	).Var(
 		"EG_ENTROPY_SEED", base64.RawURLEncoding.EncodeToString(seed),
 	).Var(
-		"EG_RESOURCES_CORES", strconv.FormatUint(numericx.Max(uint64(runtime.NumCPU()), t.runtimecfg.Cores), 10),
+		"EG_RESOURCES_CORES", strconv.FormatUint(numericx.Max(uint64(runtime.NumCPU()), t.Cores), 10),
 	).Var(
 		"EG_RESOURCES_MEMORY", strconv.FormatUint(memory, 10),
 	).Var(
-		"EG_RESOURCES_DISK", humanize.Bytes(uint64(t.runtimecfg.Disk)),
-	).CopyTo(os.Stdout)
+		"EG_RESOURCES_DISK", fmt.Sprintf("\"%s\"", string(errorsx.Zero(t.Disk.MarshalText()))),
+	)
+
+	if t.Workers > 0 {
+		environ.Var(
+			"EG_COMPUTE_WORKLOAD_CAPACITY", strconv.FormatUint(t.Workers, 10),
+		)
+	}
+
+	return environ.CopyTo(os.Stdout)
 }

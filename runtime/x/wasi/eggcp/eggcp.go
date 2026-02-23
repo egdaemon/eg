003e2a2d524f8adeddaf4fa0f0174a0138f40fc4
James Lawrence <jljatone@gmail.com>
2026-02-23 16:29:06 -0500

    disable some gcp credentials code

--- runtime/x/wasi/eggcp/eggcp.go
+++ runtime/x/wasi/eggcp/eggcp.go
@@ -3,8 +3,6 @@ package eggcp
 import (
 	"context"
 
-	_eg "github.com/egdaemon/eg"
-	"github.com/egdaemon/eg/internal/envx"
 	"github.com/egdaemon/eg/runtime/wasi/eg"
 	"github.com/egdaemon/eg/runtime/wasi/egenv"
 	"github.com/egdaemon/eg/runtime/wasi/shell"
@@ -12,15 +10,15 @@ import (
 
 // temporary hack until remap directory functionality is working.
 func CredentialsHack(ctx context.Context, o eg.Op) error {
-	encoded := envx.String("", _eg.EnvUnsafeGcloudADCB64)
-	if encoded == "" {
-		return nil
-	}
+	// encoded := envx.String("", _eg.EnvUnsafeGcloudADCB64)
+	// if encoded == "" {
+	// 	return nil
+	// }
 
 	return shell.Run(
 		ctx,
-		shell.Newf("echo %s | tr -- '-_' '+/' | base64 -d -i | install -D -m 600 /dev/stdin ~/.config/gcloud/application_default_credentials.json", encoded),
-		shell.Newf("echo %s | tr -- '-_' '+/' | base64 -d -i | install -D -m 600 /dev/stdin %s", encoded, egenv.WorkloadDirectory("gcloud", "application_default_credentials.json")),
+		// shell.Newf("echo %s | tr -- '-_' '+/' | base64 -d -i | install -D -m 600 /dev/stdin ~/.config/gcloud/application_default_credentials.json", encoded),
+		// shell.Newf("echo %s | tr -- '-_' '+/' | base64 -d -i | install -D -m 600 /dev/stdin %s", encoded, egenv.WorkloadDirectory("gcloud", "application_default_credentials.json")),
 		shell.Newf("which gcloud && gcloud config set auth/credential_file_override %s", egenv.WorkloadDirectory("gcloud", "application_default_credentials.json")),
 	)
 }

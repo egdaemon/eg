d5a3d5ed89547067f96281c4ba71721b0f8cdcc3
James Lawrence <jljatone@gmail.com>
2026-03-13 14:24:24 -0400

    add tests for gcloud agent option

--- runners/agent.option.gcloud.agent.go
+++ runners/agent.option.gcloud.agent.go
@@ -11,13 +11,17 @@ import (
 	"github.com/egdaemon/eg/internal/errorsx"
 )
 
+const (
+	EnvGoogleApplicationCredentials = "GOOGLE_APPLICATION_CREDENTIALS"
+)
+
 // configures the agent to use the current machines application default credentials.
 func AgentOptionGcloudCredentials(ctx context.Context, envb *envx.Builder, path string) AgentOption {
 	raw := errorsx.Must(os.ReadFile(path))
 	envb.Var(eg.EnvUnsafeGcloudADCB64, base64.URLEncoding.EncodeToString(raw))
 
 	// TODO: switch once eg.EnvUnsafeRemapDirectory is fully deployed.
-	envb.Var("GOOGLE_APPLICATION_CREDENTIALS", eg.DefaultWorkloadDirectory("gcloud", "application_default_credentials.json"))
+	envb.Var(EnvGoogleApplicationCredentials, eg.DefaultWorkloadDirectory("gcloud", "application_default_credentials.json"))
 
 	errorsx.Never(envb.Append(eg.EnvUnsafeRemapDirectory, eg.DefaultWorkloadDirectory("gcloud"), ":")) // if this fails it means we've introduced a change to Append that can result in an error
 

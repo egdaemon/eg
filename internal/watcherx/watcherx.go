42148f32ac633a0deb9a466f7ca89772d9b390be
James Lawrence <jljatone@gmail.com>
2026-04-21 09:16:24 -0400

    silence watch logs

--- internal/watcherx/watcherx.go
+++ internal/watcherx/watcherx.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/egdaemon/eg/internal/debugx"
 	"github.com/egdaemon/eg/internal/errorsx"
 	"github.com/radovskyb/watcher"
 )
@@ -38,7 +39,7 @@ func Proxy(ctx context.Context, src, dst string, interval time.Duration) error {
 			return nil
 		case event := <-w.Event:
 			if err := applyEvent(src, dst, event); err != nil {
-				errorsx.Log(errorsx.Wrap(err, "unable to apply file system event"))
+				debugx.Println(errorsx.Wrap(err, "unable to apply file system event"))
 			}
 		}
 	}

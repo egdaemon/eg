3f8655a4885089851a5a28bf79836deecf7d5f6a
James Lawrence <jljatone@gmail.com>
2026-08-26 07:43:19 -0400

    fix runtime migration

--- runtime/wasi/egunsafe/egworkloads/migrations/0001_workloads.sql
+++ runtime/wasi/egunsafe/egworkloads/migrations/0001_workloads.sql
@@ -1,5 +1,13 @@
+-- +goose Up
+-- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS eg_workloads_discovered (
 	id UUID PRIMARY KEY,
 	path VARCHAR NOT NULL,
 	ts TIMESTAMPTZ NOT NULL
 );
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS eg_workloads_discovered;
+-- +goose StatementEnd

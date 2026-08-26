b5c4495beec224096aecc3d60bf7f9e76e144717
James Lawrence <jljatone@gmail.com>
2026-08-26 07:50:28 -0400

    fix for genieql generation

--- runtime/wasi/egunsafe/egworkloads/00.duckdb.setup.go
+++ runtime/wasi/egunsafe/egworkloads/00.duckdb.setup.go
@@ -1,5 +1,5 @@
 package egworkloads
 
-//go:generate genieql duckdb --database=.genieql/.duckdb/egworkloads.db ./migrations
+//go:generate genieql duckdb --database=egworkloads.db ./migrations
 //go:generate genieql bootstrap --queryer=sqlx.Queryer --driver=github.com/marcboeker/go-duckdb duckdb://localhost/egworkloads.db
 //go:generate genieql auto graph -o genieql.gen.go

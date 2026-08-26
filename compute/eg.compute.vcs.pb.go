31bedf0ebb83163a0a38182f5bb6a87a2ce9ec10
James Lawrence <jljatone@gmail.com>
2026-08-26 07:52:33 -0400

    generate protobuf for gcp WIF

--- compute/eg.compute.vcs.pb.go
+++ compute/eg.compute.vcs.pb.go
@@ -194,6 +194,69 @@ func (*GitAccessCredentials_Automatic) isGitAccessCredentials_Credentials() {}
 
 func (*GitAccessCredentials_GitHttp) isGitAccessCredentials_Credentials() {}
 
+type GCPAccessCredentials struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	IdToken                   string `protobuf:"bytes,1,opt,name=id_token,proto3" json:"id_token,omitempty"`
+	Audience                  string `protobuf:"bytes,2,opt,name=audience,proto3" json:"audience,omitempty"`
+	ImpersonateServiceAccount string `protobuf:"bytes,3,opt,name=impersonate_service_account,proto3" json:"impersonate_service_account,omitempty"`
+}
+
+func (x *GCPAccessCredentials) Reset() {
+	*x = GCPAccessCredentials{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_eg_compute_vcs_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *GCPAccessCredentials) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*GCPAccessCredentials) ProtoMessage() {}
+
+func (x *GCPAccessCredentials) ProtoReflect() protoreflect.Message {
+	mi := &file_eg_compute_vcs_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use GCPAccessCredentials.ProtoReflect.Descriptor instead.
+func (*GCPAccessCredentials) Descriptor() ([]byte, []int) {
+	return file_eg_compute_vcs_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *GCPAccessCredentials) GetIdToken() string {
+	if x != nil {
+		return x.IdToken
+	}
+	return ""
+}
+
+func (x *GCPAccessCredentials) GetAudience() string {
+	if x != nil {
+		return x.Audience
+	}
+	return ""
+}
+
+func (x *GCPAccessCredentials) GetImpersonateServiceAccount() string {
+	if x != nil {
+		return x.ImpersonateServiceAccount
+	}
+	return ""
+}
+
 var File_eg_compute_vcs_proto protoreflect.FileDescriptor
 
 var file_eg_compute_vcs_proto_rawDesc = []byte{
@@ -216,8 +279,17 @@ var file_eg_compute_vcs_proto_rawDesc = []byte{
 	0x0b, 0x32, 0x21, 0x2e, 0x65, 0x67, 0x2e, 0x65, 0x67, 0x2e, 0x63, 0x6f, 0x6d, 0x70, 0x75, 0x74,
 	0x65, 0x2e, 0x47, 0x69, 0x74, 0x43, 0x72, 0x65, 0x64, 0x65, 0x6e, 0x74, 0x69, 0x61, 0x6c, 0x73,
 	0x48, 0x54, 0x54, 0x50, 0x48, 0x00, 0x52, 0x07, 0x67, 0x69, 0x74, 0x48, 0x74, 0x74, 0x70, 0x42,
-	0x0d, 0x0a, 0x0b, 0x43, 0x72, 0x65, 0x64, 0x65, 0x6e, 0x74, 0x69, 0x61, 0x6c, 0x73, 0x62, 0x06,
-	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x0d, 0x0a, 0x0b, 0x43, 0x72, 0x65, 0x64, 0x65, 0x6e, 0x74, 0x69, 0x61, 0x6c, 0x73, 0x22, 0x90,
+	0x01, 0x0a, 0x14, 0x47, 0x43, 0x50, 0x41, 0x63, 0x63, 0x65, 0x73, 0x73, 0x43, 0x72, 0x65, 0x64,
+	0x65, 0x6e, 0x74, 0x69, 0x61, 0x6c, 0x73, 0x12, 0x1a, 0x0a, 0x08, 0x69, 0x64, 0x5f, 0x74, 0x6f,
+	0x6b, 0x65, 0x6e, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x69, 0x64, 0x5f, 0x74, 0x6f,
+	0x6b, 0x65, 0x6e, 0x12, 0x1a, 0x0a, 0x08, 0x61, 0x75, 0x64, 0x69, 0x65, 0x6e, 0x63, 0x65, 0x18,
+	0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x61, 0x75, 0x64, 0x69, 0x65, 0x6e, 0x63, 0x65, 0x12,
+	0x40, 0x0a, 0x1b, 0x69, 0x6d, 0x70, 0x65, 0x72, 0x73, 0x6f, 0x6e, 0x61, 0x74, 0x65, 0x5f, 0x73,
+	0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x5f, 0x61, 0x63, 0x63, 0x6f, 0x75, 0x6e, 0x74, 0x18, 0x03,
+	0x20, 0x01, 0x28, 0x09, 0x52, 0x1b, 0x69, 0x6d, 0x70, 0x65, 0x72, 0x73, 0x6f, 0x6e, 0x61, 0x74,
+	0x65, 0x5f, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x5f, 0x61, 0x63, 0x63, 0x6f, 0x75, 0x6e,
+	0x74, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -232,11 +304,12 @@ func file_eg_compute_vcs_proto_rawDescGZIP() []byte {
 	return file_eg_compute_vcs_proto_rawDescData
 }
 
-var file_eg_compute_vcs_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
+var file_eg_compute_vcs_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_eg_compute_vcs_proto_goTypes = []interface{}{
 	(*GitCredentialsAutomatic)(nil), // 0: eg.eg.compute.GitCredentialsAutomatic
 	(*GitCredentialsHTTP)(nil),      // 1: eg.eg.compute.GitCredentialsHTTP
 	(*GitAccessCredentials)(nil),    // 2: eg.eg.compute.GitAccessCredentials
+	(*GCPAccessCredentials)(nil),    // 3: eg.eg.compute.GCPAccessCredentials
 }
 var file_eg_compute_vcs_proto_depIdxs = []int32{
 	0, // 0: eg.eg.compute.GitAccessCredentials.automatic:type_name -> eg.eg.compute.GitCredentialsAutomatic
@@ -290,6 +363,18 @@ func file_eg_compute_vcs_proto_init() {
 				return nil
 			}
 		}
+		file_eg_compute_vcs_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*GCPAccessCredentials); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	file_eg_compute_vcs_proto_msgTypes[2].OneofWrappers = []interface{}{
 		(*GitAccessCredentials_Automatic)(nil),
@@ -301,7 +386,7 @@ func file_eg_compute_vcs_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_eg_compute_vcs_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   3,
+			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
